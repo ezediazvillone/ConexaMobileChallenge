63d8d36e112bc7aae7754d731a4193dd49ed83d0
Ezequiel Diaz <ediaz@pdssa.com.ar>
2024-06-27 01:55:35 -0300

    (style) add styles, backgrounds, fonts and base application for hilt

--- app/src/main/java/com/example/conexamobilechallenge/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/conexamobilechallenge/presentation/activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.conexamobilechallenge
+package com.example.conexamobilechallenge.presentation.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.conexamobilechallenge.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
