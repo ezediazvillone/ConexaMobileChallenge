33056c19970f8a4204895db1e2698a6f22d2c996
Ezequiel Diaz <ediaz@pdssa.com.ar>
2024-06-28 20:09:07 -0300

    (fix) some ui fixes

--- app/src/main/java/com/example/conexamobilechallenge/presentation/adapter/NewsViewHolder.kt
+++ app/src/main/java/com/example/conexamobilechallenge/presentation/adapter/NewsViewHolder.kt
@@ -1,6 +1,9 @@
 package com.example.conexamobilechallenge.presentation.adapter
 
+import android.content.Context
+import android.util.DisplayMetrics
 import android.view.View
+import android.view.WindowManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.conexamobilechallenge.databinding.ItemNewsBinding
@@ -16,7 +19,21 @@ class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     ) {
         setNewsTitle(news.title)
         setNewsImage(news.image)
+        setNewsHeight()
         binding.itemNewsIv.setOnClickListener { onNewsClick(news) }
+    }
+
+    private fun setNewsHeight() {
+        val displayMetrics = DisplayMetrics()
+        val windowManager = binding.itemNewsCv.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val height = displayMetrics.heightPixels
+
+        val height30Percent = (height * 0.30).toInt()
+
+        val params = binding.itemNewsCv.layoutParams
+        params.height = height30Percent
+        binding.itemNewsCv.layoutParams = params
     }
 
     private fun setNewsImage(image: String) {
